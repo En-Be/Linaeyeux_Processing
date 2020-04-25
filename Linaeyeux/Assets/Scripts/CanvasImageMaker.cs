f3e9d4ca1936807e16807aa674c47a58fe7d1a74
Nicholas <n3ttl3t@gmail.com>
2020-04-25 17:13:04 +0100

    Blank canvas generated to fit screen

---
+++ Linaeyeux/Assets/Scripts/CanvasImageMaker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasImageMaker : MonoBehaviour {
+    private Canvas canvas;
+    private float width;
+    private float height;
+
+    private void Start () 
+    {
+        SetParams();
+        MakeAndAssignImage();  
+    }
+
+    private void SetParams()
+    {
+        canvas = GetComponent<Canvas>();
+        width = Screen.width;
+        height = Screen.height;
+    }
+
+    private void MakeAndAssignImage()
+    {
+        GameObject newObject = new GameObject("ObjectName");
+        RectTransform rectTransform = newObject.AddComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(width, height);
+        Image image = newObject.AddComponent<Image>();
+        newObject.transform.SetParent(canvas.transform, false);
+    }
+};

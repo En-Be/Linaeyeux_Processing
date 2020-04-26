db2a493b115543777b1dad9e8689f336de779047
Nicholas <n3ttl3t@gmail.com>
2020-04-26 01:07:31 +0100

    tiles animating through flat colours quicker

--- Linaeyeux/Assets/Scripts/Tiles.cs
+++ Linaeyeux/Assets/Scripts/Tiles.cs
@@ -7,34 +7,44 @@ public class Tiles : MonoBehaviour
 {
     private RawImage rawImage;
     private Texture2D texture;
+    private int tileHeight;
+    private int tileWidth;
 
     public void Setup()
     {
         rawImage = GetComponentInChildren<RawImage>();
         texture = rawImage.texture as Texture2D;
+        SplitIntoTiles();
     }
 
     public void MakeTiles()
     {
-        // var data = texture.GetRawTextureData<Color32>();
-
-        // int index = 0;
-        Color32 colour = new Color32(
-            (byte)Random.Range(0, 255), 
-            (byte)Random.Range(0, 255), 
-            (byte)Random.Range(0, 255), 
-            (byte)Random.Range(0, 255));
-
-        for (int y = 0; y < texture.height; y++)
+        for (int ty = 0; ty < 4; ty++)
         {
-            for (int x = 0; x < texture.width; x++)
+            for (int tx = 0; tx < 4; tx++)
             {
+                Color32 colour = new Color32(
+                    (byte)Random.Range(0, 255), 
+                    (byte)Random.Range(0, 255), 
+                    (byte)Random.Range(0, 255), 
+                    (byte)Random.Range(0, 255));
 
-
-                // data[index++] = colour;
-                texture.SetPixel(x, y, colour);
+                for (int y = ty * tileHeight; y < (ty + 1) * tileHeight; y++)
+                {
+                    for (int x = tx * tileWidth; x < (tx + 1) * tileWidth; x++)
+                    {
+                        texture.SetPixel(x, y, colour);
+                    }
+                }
+                texture.Apply(false);
             }
         }
-        texture.Apply(false);
+    }
+
+    private void SplitIntoTiles()
+    {
+        tileHeight = texture.height / 4;
+        tileWidth = texture.width / 4;
+        Debug.Log($"tile height = {tileHeight}, tile width = {tileWidth}");
     }
 }
